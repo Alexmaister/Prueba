220bcfe505db24c454b485e0986a784a5bccbb05
Alexmaister <aortizurita@gmail.com>
2017-02-11 03:54:08 +0100

    empezada fecha

---
+++ AplicacionVeterinaria/Clases/Fecha.java
@@ -0,0 +1,7 @@
+package Clases;
+
+/**
+ * Created by Ale on 10/02/2017.
+ */
+public class Fecha {
+}
---
+++ AplicacionVeterinaria/Excepciones/ExcepcionFecha.java
@@ -0,0 +1,7 @@
+package Excepciones;
+
+/**
+ * Created by Ale on 11/02/2017.
+ */
+public class ExcepcionFecha {
+}
