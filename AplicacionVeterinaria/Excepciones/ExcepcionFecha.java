6f0a0d6ced37f702ba6502e6852b5f82986735f9
Alexmaister <aortizurita@gmail.com>
2017-02-20 22:02:19 +0100

    fecha

--- AplicacionVeterinaria/Excepciones/ExcepcionFecha.java
+++ AplicacionVeterinaria/Excepciones/ExcepcionFecha.java
@@ -3,5 +3,8 @@ package Excepciones;
 /**
  * Created by Ale on 11/02/2017.
  */
-public class ExcepcionFecha {
+public class ExcepcionFecha extends Exception{
+    public ExcepcionFecha(String mensaje){
+        System.out.print(mensaje);
+    }
 }
