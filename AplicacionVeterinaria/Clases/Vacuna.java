2edac9bec25894c87bc11cbde2d6062f7aea7bfe
Alexmaister <aortizurita@gmail.com>
2017-03-22 13:31:22 +0100

    atributos y constructor por defecto vacuna

--- AplicacionVeterinaria/Clases/Vacuna.java
+++ AplicacionVeterinaria/Clases/Vacuna.java
@@ -1,7 +1,25 @@
 package Clases;
+import Clases.Enfermedad;
+import java.util.*;
 
 /**
  * Created by aortiz on 22/03/2017.
  */
 public class Vacuna {
+
+    private String nombre;
+    private Double dosis;
+    private List<Enfermedad> enfermedades;
+    private double concentracion;
+    private String agenteAc;
+
+    Vacuna(){
+        nombre="Rabia";
+        dosis=0.50;
+        enfermedades=new ArrayList<Enfermedad>();
+        enfermedades.add(new Enfermedad());
+        concentracion=0.5;
+        agenteAc="Rubiminuoxitona";
+
+    }
 }
