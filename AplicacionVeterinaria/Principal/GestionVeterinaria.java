e00a2d6cb022255d6adb35af478c368da57e573e
Alexmaister <aortizurita@gmail.com>
2017-04-03 13:08:12 +0200

    ok

---
+++ AplicacionVeterinaria/Principal/GestionVeterinaria.java
@@ -0,0 +1,14 @@
+package Principal;
+
+/**
+ * Created by aortiz on 03/04/2017.
+ */
+public class GestionVeterinaria {
+
+    /*cabecera: public static void mostrarMenuPrincipal
+    descripcion: procedimiento que mostrara en pantalla una serie de opciones
+    * */
+    public static void mostrarMenuPrincipal(){
+        Syste
+    }
+}
---
+++ AplicacionVeterinaria/Principal/Veterinaria.java
@@ -0,0 +1,22 @@
+package Principal;
+
+/**
+ * Created by aortiz on 03/04/2017.
+ *
+ * Clase principal ,contendra el metodo main de la aplicacion
+ * que maneja clientes y sus mascotas
+ */
+
+/*
+* Analisis:
+* Programa que mostrara un menu de opciones en el cual se podra mostrar una lista de personas
+* ,las mascota de alguna persona,
+* añadir ,borrar alguna mascota y
+* clientes tambien.
+* */
+
+/*
+* */
+
+public class Veterinaria {
+}
