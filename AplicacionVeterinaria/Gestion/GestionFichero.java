b5ae7faaa4a3aedcefce1845191fc1876cebbce4
Alexmaister <aortizurita@gmail.com>
2017-04-05 01:56:46 +0200

    file

---
+++ AplicacionVeterinaria/Gestion/GestionFichero.java
@@ -0,0 +1,33 @@
+package Gestion;
+import java.io.*;
+import Clases.*;
+/**
+ * Created by Ale on 05/04/2017.
+ */
+public class GestionFichero {
+
+    private File fichero;
+
+    public File getFichero(){
+        return this.fichero;
+    }
+    public void setFichero(String path){
+        this.fichero=new File(path);
+    }
+    public static void main(String... a){
+
+        GestionFichero gf=new GestionFichero();
+        gf.setFichero("FicheroPrueba.txt");
+        try {
+            PrintWriter pw=new PrintWriter(gf.getFichero());
+
+            pw.println(new Persona());
+
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ AplicacionVeterinaria/Test/FicherosTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * Created by Ale on 05/04/2017.
+ */
+public class FicherosTest {
+
+    public static void main(String...a){
+
+    }
+}
