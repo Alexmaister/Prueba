6e4d2016fb84de1692c201de11000dbb3c01f074
Alexmaister <aortizurita@gmail.com>
2017-05-19 02:35:49 +0200

    i

--- AplicacionVeterinaria/GestionFicheros/FicheroDiario.java
+++ AplicacionVeterinaria/GestionFicheros/FicheroDiario.java
@@ -53,18 +53,18 @@ public class FicheroDiario /*extends Diario*/{
         }
         return registros;
     }
-    /*cabecra: void guardarPersona(Persona p)
+    /*cabecra: void guardarPersona(Persona p)throws ExcepcionDiario 
     * descripcion: procedimiento que escribira en la posicion deseada del fichero diario una  persona de quererse incluir
     * en la posicion final , establezca pos como una llamada a la funcion contarRegistrosPersona
     * entradas:una persona,
     * precondiciones:ninguna*/
     public void  guardarPersona (Persona p,int pos)throws ExcepcionDiario {
         if(pos<0){throw new ExcepcionDiario("Debe escribir en una posicion correcta del archivo");}else{
-        int posicion=pos * bytesP.total.getBytes();
+                validarAtributosPersona(p);
+            int posicion=pos * bytesP.total.getBytes();
         try {
             flujo = new RandomAccessFile(diario, "rw");
             flujo.seek(posicion);
-            validarAtributosPersona(p);
             flujo.writeBytes(p.getNombre());
             flujo.seek(posicion+bytesP.ape.getBytes());
             flujo.writeBytes(p.getApellidos());
@@ -88,7 +88,7 @@ public class FicheroDiario /*extends Diario*/{
         }
     }
 
-    /*cabecera: void guardarMascota(Mascota m, int pos)
+    /*cabecera: void guardarMascota(Mascota m, int pos)throws ExcepcionDiario 
     descripcion: procedimiento que guardara una mascota en el archivo de diarioMascota en la posicion indicada por parametros
     entradas: objeto mascota, un entero
     precondiciones:ninguna
@@ -128,21 +128,69 @@ public class FicheroDiario /*extends Diario*/{
 
     }
 
-    /*cabecera: public void modificarAtributosPersona(String atributo,int at,int pos)
+    /*cabecera: void modificarAtributosPersona(String atributo,int at,int pos)throws ExcepcionDiario 
     descripcion: procedimiento que modificara el atributo deseado de una persona
     entradas: String , dos enteros
     precondiciones:ninguna
     * */
     public void modificarAtributosPersona(String atributo,int at,int pos)throws ExcepcionDiario{
-        if((at<1 || at>9)&&(pos<0)){throw new ExcepcionDiario("");}else{
+        if((at<1 || at>9)||(pos<0)){throw new ExcepcionDiario("");}else{
+                if(at==1 || at==2 || at==7)
+                    atributo=String.format("%1$-20",atributo);
+            try{
 
+                flujo=new RandomAccessFile(diario,"r");
+                flujo.seek((pos*bytesP.total.getBytes())+(pasarNumeroAtributo(at)));
+               // flujo.writeBytes();
+
+            }catch(IOException e){}
 
 
         }
     }
-    /*
+    /*cabecera:int pasarNumeroAtributo(int pos)
+    descripcion: funcion que calculara la posicion en bytes en la cual debe escribirse un determinado atributo 1-9
+    entradas: un entero
+    precondiciones:no hay
+    salidas: un entero
+    postcondiciones:se devolvera asociado al nombre la posicion en la que debe escribirse un de terminado atributo,de no ser correcto el atributo
+    se devolvera -1
     * */
-    private int pasarNumeroAtributo(int pos){return bytesM.nom.getBytes();}
+    private int pasarNumeroAtributo(int pos){
+        int b=0;
+        switch (pos){
+            case 1:
+                b=bytesP.nom.getBytes();
+                break;
+            case 2:
+
+                b=bytesP.ape.getBytes();
+                break;
+            case 3:
+                b=bytesP.f.getBytes();
+                break;
+            case 4:
+                b=bytesP.DNI.getBytes();
+                break;
+            case 5:
+                b=bytesP.Tl.getBytes();
+            case 6:
+                b=bytesP.Mvl.getBytes();
+                break;
+            case 7:
+                b=bytesP.Direc.getBytes();
+                break;
+            case 8:
+                b=bytesP.codP.getBytes();
+                break;
+            case 9:
+                b=bytesP.nac.getBytes();
+                break;
+            default:
+                b=-1;
+                break;
+        }
+        return b;}
 
 
     /*cabecera: void validarAtributosPersona(Persona p)
@@ -159,13 +207,22 @@ public class FicheroDiario /*extends Diario*/{
         : (( p.getApellidos().length()>bytesP.ape.getBytes() ))
         ? p.setApellidos(p.getApellidos().substring(bytesP.f.getBytes()-bytesP.ape.getBytes()))
         :0);*/
-        if(( p.getNombre().length()>bytesP.ape.getBytes() )&& ( p.getApellidos().length()>bytesP.ape.getBytes())  ) {
+        if(( p.getNombre().length()>bytesP.ape.getBytes() )&& ( p.getApellidos().length()>bytesP.ape.getBytes()) && (p.getDireccion().length()>(bytesP.codP.getBytes()-bytesP.Direc.getBytes())) ) {
             p.setNombre(p.getNombre().substring(0,bytesP.ape.getBytes()-1));
             p.setApellidos(p.getApellidos().substring(0,bytesP.ape.getBytes()-1));
-        }else if(p.getNombre().length()>bytesP.ape.getBytes()){
+            p.setDireccion(p.getDireccion().substring(0,bytesP.codP.getBytes()-bytesP.Direc.getBytes()));
+        }else if(p.getNombre().length()>bytesP.ape.getBytes())
             p.setNombre(p.getNombre().substring(0, bytesP.ape.getBytes()-1));
-        }else if  ( p.getApellidos().length()>bytesP.ape.getBytes()){
+        else if  ( p.getApellidos().length()>bytesP.ape.getBytes())
         p.setApellidos(p.getApellidos().substring(0,bytesP.ape.getBytes()-1));
+        else if(p.getDireccion().length()>(bytesP.codP.getBytes()-bytesP.Direc.getBytes()))
+            p.setDireccion(p.getDireccion().substring(0,bytesP.codP.getBytes()-bytesP.Direc.getBytes()));
+        else{
+            p.setNombre(String.format("%1$-20",p.getNombre()));
+            p.setApellidos(String.format("%1$-20",p.getApellidos()));
+            p.setDireccion(String.format("%1$-20",p.getDireccion()));
         }
     }
+	
+
 }
