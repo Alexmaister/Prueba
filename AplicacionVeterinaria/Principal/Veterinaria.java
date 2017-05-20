321e61117789b03bdf7367ef6dc49003de847fe9
Alexmaister <aortizurita@gmail.com>
2017-05-20 16:21:21 +0200

    probando ventana

--- AplicacionVeterinaria/Principal/Veterinaria.java
+++ AplicacionVeterinaria/Principal/Veterinaria.java
@@ -1,4 +1,8 @@
 package Principal;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 
 /**
  * Created by aortiz on 03/04/2017.
@@ -18,5 +22,24 @@ package Principal;
 /*
 * */
 
-public class Veterinaria {
+public class Veterinaria extends JFrame {
+
+    public Veterinaria(){
+        setVisible(true);//por defecto es false
+        setSize(1000,500);// por defecto 0,0
+        setLocation(2500,200);// por defecto 0,0
+        //setBound sirve x los dos anteriores
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setIconImage(new Image() );
+        setTitle("Veterinaria");
+        setResizable(false);//por defecto es true
+        setExtendedState(Frame.MAXIMIZED_BOTH);//pantalla completa , se abre asi
+
+    }
+
+    public static void main(String... a){
+
+        Veterinaria v= new Veterinaria();
+
+    }
 }
